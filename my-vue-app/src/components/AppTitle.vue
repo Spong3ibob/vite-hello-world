f4d97809a4577eb0e88029ef8bffd23da74a9b3a
Spong3ibob <leonardomaran17@gmail.com>
2022-11-23 16:37:45 +0100

    aggiunto nuovo apptitle

---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title:"La mia prima app con Vitae!"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<style scoped>
+h1{
+    color: coral;
+}
+</style>
